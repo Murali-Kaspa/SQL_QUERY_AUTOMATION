c9fc6897d5b53a84c8a1994bea16640e3630dd05
EC2 Default User <ec2-user@ip-172-31-5-191.ap-south-1.compute.internal>
2026-03-23 14:03:44 +0000

    Created New DB ad Table i.e., OnePLus

---
+++ SQL_DDL/04_create_tables.sql
@@ -0,0 +1,37 @@
+-- SQL_DDL/02_create_tables.sql
+
+USE OnePlus;
+
+-- Employees Table
+CREATE TABLE IF NOT EXISTS employees (
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE,
+    department VARCHAR(100),
+    salary DECIMAL(10,2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Departments Table
+CREATE TABLE IF NOT EXISTS departments (
+    dept_id INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(100) UNIQUE,
+    location VARCHAR(100)
+);
+
+-- Projects Table
+CREATE TABLE IF NOT EXISTS projects (
+    project_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(150),
+    emp_id INT,
+    start_date DATE,
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS branches (
+    branch_id INT AUTO_INCREMENT PRIMARY KEY,
+    building_name VARCHAR(100),
+    block VARCHAR(10),
+    department VARCHAR(50)
+);
